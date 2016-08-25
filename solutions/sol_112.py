31c59612cd5d8af4ceed6f6770622117ffa5e0cf
Valerio Maggio <valerio.maggio@gmail.com>
2016-08-25 12:30:52 +0200

    Fixed a typo in the solution

--- solutions/sol_112.py
+++ solutions/sol_112.py
@@ -1,4 +1,4 @@
 ann = ANN(2, 10, 1)
-%timeit -n 1 -r 1 ann.train(zip(X,y), iterations=50)
+%timeit -n 1 -r 1 ann.train(zip(X,y), iterations=100)
 plot_decision_boundary(ann)
-plt.title("Our model with 10 hidden units and 10 iterations")
+plt.title("Our model with 10 hidden units and 100 iterations")
