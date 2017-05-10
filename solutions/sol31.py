fc4d73f8da9dbd1652c52d1fa91a1550bac85876
Valerio Maggio <valerio.maggio@gmail.com>
2017-05-10 06:28:49 +0000

    Refined CNN on Cifar + moved section on CNN weights in CNN Notebook. Added hint on get_activations for hidden features

--- solutions/sol31.py
+++ solutions/sol31.py
@@ -3,14 +3,14 @@ from keras.layers.core import Dense, Dropout, Flatten, Activation
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.optimizers import Adadelta
 
-input_shape = (3, 32, 32)
-nb_classes = 10
+# This solution assumes shape_ord 
+# for input_shape has been aready defined
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=input_shape))
+                 input_shape=shape_ord))
 model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
+model.add(Conv2D(32, (3, 3)))  # padding='valid' is default
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
